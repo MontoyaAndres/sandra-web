bbfbae34847ffd2148d3a26fcfa541a179af82fb
MontoyaAndres <andresmontoyafcb@gmail.com>
2019-07-18 18:53:21 -0500

    remove limit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,16 +14,7 @@ function index() {
     inputValue: "",
     selectValue: selectValues[0]
   });
-  const [limit, setLimit] = React.useState(9);
-  const [isButtonHide, setIsButtonHide] = React.useState(false);
-
-  const { products } = useSearch(values, limit);
-
-  React.useEffect(() => {
-    if (limit > products.length) {
-      setIsButtonHide(true);
-    }
-  }, [limit]);
+  const { products } = useSearch(values);
 
   return (
     <>
@@ -39,17 +30,6 @@ function index() {
         itemsSelectList={selectValues}
         products={products}
       />
-
-      {!isButtonHide && (
-        <div className="content has-text-centered">
-          <button
-            className="button is-primary is-large"
-            onClick={() => setLimit(limit + 9)}
-          >
-            Ver más
-          </button>
-        </div>
-      )}
     </>
   );
 }
--- utils/useSearch.tsx
+++ utils/useSearch.tsx
@@ -7,21 +7,18 @@ interface SearchValue {
   selectValue: string;
 }
 
-function useSearch(values: SearchValue, limit: number) {
-  const { inputValue, selectValue } = values;
+function useSearch({ inputValue, selectValue }: SearchValue) {
   const [products, setProducts] = useState([]);
 
   useMemo(() => {
-    const result = getJSONToRead(selectValue)
-      .slice(0, limit)
-      .filter(value =>
-        `${value.name} ${value.description}`
-          .toLowerCase()
-          .includes(inputValue.toLowerCase())
-      );
+    const result = getJSONToRead(selectValue).filter(value =>
+      `${value.name} ${value.description}`
+        .toLowerCase()
+        .includes(inputValue.toLowerCase())
+    );
 
     setProducts(result);
-  }, [inputValue, selectValue, limit]);
+  }, [inputValue, selectValue]);
 
   return { products };
 }
